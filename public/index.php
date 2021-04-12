d15e08a4e0d43a8dbde4201e2a544c699fac8a52
fg-yarullin <failneke@gmail.com>
2021-04-12 12:57:12 +0300

    gitignore edited

--- public/index.php
+++ public/index.php
@@ -5,27 +5,3 @@ require __DIR__ . '/../config/config.php';
 // require __DIR__ . '/../vendor/autoload.php';
 
 require_once __DIR__ . '/../app/bootstrap.php';
-
-// twig
-// include __DIR__ . '/../app/views/first.php';
-
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
-use Monolog\Formatter\HtmlFormatter;
-
-// Создаем логгер 
-$log = new Logger('mylog');
-
-// Хендлер, который будет писать логи в "mylog.log" и слушать все ошибки с уровнем "WARNING" и выше .
-$log->pushHandler(new StreamHandler('mylog.log', Logger::WARNING));
-
-// Хендлер, который будет писать логи в "troubles.log" и реагировать на ошибки с уровнем "ALERT" и выше.
-$log->pushHandler(new StreamHandler('troubles.log', Logger::ALERT));
-
-$log->pushHandler(new StreamHandler('troubles.log', Logger::INFO));
-
-
-// Добавляем записи
-$log->warning('Предупреждение');
-$log->error('Большая ошибка');
-$log->info('Просто тест');
